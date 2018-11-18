1de587a7bed844cb0fb818dceca1e9e34f92243c
limbudenil <limbudenil@gmail.com>
2018-11-18 16:14:17 +0000

    Add table and render data from model hook

---
+++ app/routes/sample.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model(){
+
+		return [ 
+			   		{firstName: "Denil.....................................................................jpg", lastName: "Limbu"},
+		         	{firstName:"COCOA.......ghk", lastName: "CyberPunk"},
+		         	{firstName: "differenet.png", lastName: "honi"},
+		         	{firstName:"ktha.word", lastName: "mul"}
+		];
+	}
+});
